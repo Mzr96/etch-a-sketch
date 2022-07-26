fdee80dfb9be99b88533f1e8e7b3462de65ff87b
Maziar <maziarmoslemi@gmail.com>
2022-07-26 19:55:01 +0430

    Add sizing and rest buttons

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 'use strict'
 
 const grid = document.querySelector('.grid');
+const sizeButtons = document.querySelectorAll('.set-size')
+const btnReset = document.querySelector('.reset')
+let gridSize = 64;
 
+const createGrid = function () {
+    for (let i = 0; i < gridSize; i++) {
+        const rowGrid = document.createElement("div");
+        rowGrid.classList.add("row", `row--${i + 1}`);
+        rowGrid.style.height = `${560 / gridSize}px`
+        for (let j = 0; j < gridSize; j++) {
+            const cellGrid = document.createElement("div");
+            cellGrid.classList.add("cell", `cell--${i + 1}-${j + 1}`);
+            cellGrid.style.height = `${560 / gridSize}px`
+            cellGrid.style.width = `${560 / gridSize}px`
+            rowGrid.appendChild(cellGrid);
+            cellGrid.addEventListener('mouseover', function (e) {
+                e.target.classList.add('hovered')
+            })
+        }
+        grid.appendChild(rowGrid)
 
-for (let i = 0; i < 64; i++) {
-    const rowGrid = document.createElement("div");
-    rowGrid.classList.add("row", `row--${i + 1}`);
-    for (let j = 0; j < 64; j++) {
-        const cellGrid = document.createElement("div");
-        cellGrid.classList.add("cell", `cell--${i + 1}-${j + 1}`);
-        rowGrid.appendChild(cellGrid);
-        cellGrid.addEventListener('mouseover', function (e) {
-            e.target.classList.add('hovered')
-        })
+    
     }
-    grid.appendChild(rowGrid)
+    grid.classList.add('full')
 }
 
+const resetGrid = function () {
+    if (grid.classList.contains('full')) {
+        const rows = document.querySelectorAll('.row')
+        rows.forEach(row => row.remove())
+        grid.classList.remove('full')
+    }
+} 
+
+sizeButtons.forEach(btn => btn.addEventListener('click', function (e) {
+    if (e.target.classList.contains('small')) {
+        gridSize = 16;
+        resetGrid()
+        createGrid();
+    } else if (e.target.classList.contains('medium')) {
+        gridSize = 32;
+        resetGrid()
+        createGrid();
+    } else if (e.target.classList.contains('large')) {
+        gridSize = 64;
+        resetGrid()
+        createGrid();
+    }
+}))
+
+btnReset.addEventListener('click', resetGrid)
